80c59ec2ed43be577558bb3b6cebc43ac0a5a406
Stephen Akinyemi <appcypher@outlook.com>
2026-03-19 17:49:38 +0100

    fix: restore ci for 0.1.7 bump

--- src/cpuid/src/common.rs
+++ src/cpuid/src/common.rs
@@ -34,14 +34,14 @@ pub fn get_cpuid(function: u32, count: u32) -> Result<CpuidResult, Error> {
         }
     }
 
-    let max_function = unsafe { __get_cpuid_max(function & leaf_0x80000000::LEAF_NUM) }.0;
+    let max_function = __get_cpuid_max(function & leaf_0x80000000::LEAF_NUM).0;
     if function > max_function {
         return Err(Error::InvalidParameters(format!(
             "Function not supported: 0x{function:x}"
         )));
     }
 
-    let entry = unsafe { __cpuid_count(function, count) };
+    let entry = __cpuid_count(function, count);
     if entry.eax == 0 && entry.ebx == 0 && entry.ecx == 0 && entry.edx == 0 {
         return Err(Error::InvalidParameters(format!("Invalid count: {count}")));
     }
